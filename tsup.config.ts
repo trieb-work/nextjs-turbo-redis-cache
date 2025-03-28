2dffad68401bc273cf81a0a0d06446d34b574a5e
Tilman Marquart <til.marquart@gmail.com>
2025-03-28 21:31:39 +0100

    feat: add tsup

---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  outDir: 'dist',
+  format: ['esm', 'cjs'],
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  target: 'es2020',
+  shims: false,
+  minify: false,
+});
