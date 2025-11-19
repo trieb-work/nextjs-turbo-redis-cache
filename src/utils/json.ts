5a43493c53370354ae3238cf589abfe2c6080c41
Martin Schalter <schalter.dev@gmail.com>
2025-11-19 17:40:42 +0100

    fix: serialize map into json

--- src/utils/json.ts
+++ src/utils/json.ts
@@ -1,12 +1,25 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function bufferReviver(_: string, value: any): any {
+export function bufferAndMapReviver(_: string, value: any): any {
   if (value && typeof value === 'object' && typeof value.$binary === 'string') {
     return Buffer.from(value.$binary, 'base64');
+  }
+  if (
+    value &&
+    typeof value === 'object' &&
+    typeof value.$map === 'object' &&
+    !!value.$map
+  ) {
+    return new Map(
+      Object.entries(value.$map).map(([key, value]) => {
+        const revivedValue = bufferAndMapReviver('', value);
+        return [key, revivedValue];
+      }),
+    );
   }
   return value;
 }
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function bufferReplacer(_: string, value: any): any {
+export function bufferAndMapReplacer(_: string, value: any): any {
   if (Buffer.isBuffer(value)) {
     return {
       $binary: value.toString('base64'),
@@ -20,6 +33,16 @@ export function bufferReplacer(_: string, value: any): any {
   ) {
     return {
       $binary: Buffer.from(value.data).toString('base64'),
+    };
+  }
+  if (value && typeof value === 'object' && value instanceof Map) {
+    return {
+      $map: Object.fromEntries(
+        Array.from(value.entries()).map(([key, value]) => {
+          const replacedValue = bufferAndMapReplacer('', value);
+          return [key, replacedValue];
+        }),
+      ),
     };
   }
   return value;
