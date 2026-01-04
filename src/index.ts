215d185b331fcce66533af2f20ac0f2f29bf01d3
Jannik Zinkl <jannik@trieb.work>
2026-01-04 23:59:04 +0100

    fix: export CreateRedisStringsHandlerOptions type

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import CachedHandler from './CachedHandler';
 export default CachedHandler;
 import RedisStringsHandler from './RedisStringsHandler';
 export { RedisStringsHandler };
+export type { CreateRedisStringsHandlerOptions } from './RedisStringsHandler';
